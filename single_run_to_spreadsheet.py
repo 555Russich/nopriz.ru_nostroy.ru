83514567f2af3c64f98072c900b2f52925305ac2
Dmitriy Kutsokon <Kutsokondima@yandex.ru>
2024-03-07 01:28:46 +0300

    add

---
+++ single_run_to_spreadsheet.py
@@ -0,0 +1,16 @@
+import asyncio
+from datetime import datetime, timedelta
+
+from main import scrap_all
+from my_logging import get_logger
+
+
+async def main():
+    date_to = datetime.now()
+    date_from = date_to - timedelta(days=5)
+    await scrap_all(date_from=date_from, date_to=date_to)
+
+
+if __name__ == '__main__':
+    get_logger('single_run_to_spreadsheet.log')
+    asyncio.run(main())
